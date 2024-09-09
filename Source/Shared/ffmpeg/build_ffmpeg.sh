4684335b9a1e5ef5243b9ef683880ab939ba145b
Timothy Schoen <timschoen123@gmail.com>
2024-09-09 13:57:52 +0200

    FFMpeg build fixes

--- Source/Shared/ffmpeg/build_ffmpeg.sh
+++ Source/Shared/ffmpeg/build_ffmpeg.sh
@@ -4,17 +4,21 @@
 FFMPEG_DIR="ffmpeg-7.0.1"
 OS=$(uname)
 
+
 # Define platform-specific configurations
 if [[ "$OS" == "Darwin" ]]; then
     ffmpeg_config="--extra-cflags=-mmacosx-version-min=10.9 --extra-ldflags=-mmacosx-version-min=10.9"
     ffmpeg_cc="clang -arch x86_64 -arch arm64"
+    sed -i.bak '5021c\
+            _DEPCMD='\''$(DEP$(1)) $(DEP$(1)FLAGS) $($(1)DEP_FLAGS) $< 2>&1 | grep "^Note:.*file:" | sed -e "s^.*file: *^$@: ^" | tr \\\\\\\\/ \/ > $(@:.o=.d)'\'' '$'\n' "$FFMPEG_DIR/configure"
 elif [[ "$OS" == "Linux" ]]; then
     ffmpeg_config="--enable-pic"
     ffmpeg_cc="${CC:-gcc}"
 elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW64_NT" ]; then
     ffmpeg_config="--toolchain=msvc"
     ffmpeg_cc="${CC:-gcc}"
-    sed -i '/_DEPCMD=/c\_DEPCMD='\''$(DEP$(1)) $(DEP$(1)FLAGS) $($(1)DEP_FLAGS) $< 2>&1 | grep "^Note:.*file:" | sed -e "s^.*file: *^$@: ^" | tr \\\\ / > $(@:.o=.d)'\''' "$FFMPEG_DIR/configure"
+    sed -i.bak '5021c\
+            _DEPCMD='\''$(DEP$(1)) $(DEP$(1)FLAGS) $($(1)DEP_FLAGS) $< 2>&1 | grep "^Note:.*file:" | sed -e "s^.*file: *^$@: ^" | tr \\\\\\\\/ \/ > $(@:.o=.d)'\'' '$'\n' "$FFMPEG_DIR/configure"
 else
     echo "Unsupported OS: $OS"
     exit 1
